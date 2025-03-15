2748ce4549bf1dbc9dc4be4ce095a57ae999dcd6
prajyot7070 <maneprajyot756@gmail.com>
2025-03-15 22:10:49 +0530

    fixed cors

--- dist/index.js
+++ dist/index.js
@@ -8,15 +8,11 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const trinoRoutes_1 = __importDefault(require("./routes/trinoRoutes"));
 const aiRoutes_1 = __importDefault(require("./routes/aiRoutes"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 3003;
 const app = (0, express_1.default)();
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
+app.use((0, cors_1.default)());
 // Middleware
 app.use(express_1.default.json());
 // Routes
--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,8 @@ dotenv.config();
 const PORT = process.env.PORT || 3003;
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(cors());
+
 
 // Middleware
 app.use(express.json());
